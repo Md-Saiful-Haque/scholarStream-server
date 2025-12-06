89c8249ac6d20ad9d7638e6986ab17deade9e8e3
Md Saiful Haque <saifulhaque.pciu@gmail.com>
2025-12-06 21:30:10 +0600

    user created in the database

--- index.js
+++ index.js
@@ -8,28 +8,70 @@ const port = process.env.PORT || 3000
 
 
 app.use(express.json())
-app.use(cors())
+app.use(
+  cors({
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:5174'
+    ],
+    credentials: true,
+    optionSuccessStatus: 200,
+  })
+)
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nmodl3i.mongodb.net/?appName=Cluster0`;
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
+
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    //await client.close();
-  }
+    try {
+        await client.connect();
+
+        const db = client.db('scholarStreamDB')
+        const usersCollection = db.collection('users')
+        const scholarshipsCollection = db.collection('scholarships')
+        const applicationsCollection = db.collection('applications')
+        const reviewsCollection = db.collection('reviews')
+
+        // users related apis
+        app.post('/users', async (req, res) => {
+            const userInfo = req.body;
+            userInfo.role = 'Student';
+            userInfo.createdAt = new Date()
+console.log(userInfo)
+            const email = userInfo.email
+            const userExists = await usersCollection.findOne({ email })
+            if (userExists) {
+                return res.send({ message: 'user already exists' })
+            }
+
+            const result = await usersCollection.insertOne(userInfo)
+            res.send(result)
+        })
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        //await client.close();
+    }
 }
 run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello from Scholar Stream Server..')
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
