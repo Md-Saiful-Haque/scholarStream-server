a1d20091f56d3674f2b2dea8567a083e48bf8bd0
Md Saiful Haque <saifulhaque.pciu@gmail.com>
2025-12-16 01:27:01 +0600

    delete operation

--- index.js
+++ index.js
@@ -422,6 +422,20 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/delete-myReview/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await reviewsCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        app.delete('/delete-review/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await reviewsCollection.deleteOne(query)
+            res.send(result)
+        })
+
 
 
 
